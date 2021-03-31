5cbaa7e3055d2c7bd64740f4430ef9895d3f823a
Matthew Covington <mcovington1@morbius.mscsnet.mu.edu>
2021-03-30 22:57:26 -0500

    final changes, fingers crossed

--- xinu-hw7/include/version.h
+++ xinu-hw7/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi3) #300 (mcovington1@morbius) Tue Mar 30 22:52:26 CDT 2021"
+#define VERSION "(Embedded Xinu) (arm-rpi3) #301 (mcovington1@morbius) Tue Mar 30 22:56:38 CDT 2021"
